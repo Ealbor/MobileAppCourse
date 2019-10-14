71b765b0aa6683fa5cb32c58e6077c2cd0fa8c68
Eric <ealbor@my.wctc.edu>
2019-10-14 16:40:06 -0500

    GestureSwipeLab
    I tried to have 2 box views overlap a label. The point was to have a label be swipeable and trigger one of 2 commands connected to the action.
    But because they overlapped, they seemed to conflict with one another.
    
    As a result, I had to split the label, with a color code to indicate which side corresponds with what.

---
+++ GestureLab/GestureLab/MainViewModel.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+
+namespace GestureLab
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+
+        public ICommand YesCommand { get; }
+        public ICommand NoCommand { get; }
+        public ICommand Restart { get; }
+
+
+
+        //Points that use determine user character. 
+        int points = 0;
+
+        //Keeps track of question number
+        int questionSequence = 0;
+
+        //A test
+        string currentQuestion = "Do you like SciFi?";
+
+        //Going to hold the image URL set to the source of the image element
+        string imageUrl = "";
+
+
+        public MainViewModel()
+        {
+            YesCommand = new Command(IncreasePoints);
+            NoCommand = new Command(LeavePoints);
+            Restart = new Command(RestartApp);
+        }
+
+
+
+        void RestartApp()
+        {
+            (Application.Current).MainPage = new MainPage();
+        }
+
+
+        //1 of 2 methods called by commands
+        void IncreasePoints()
+        {
+            if (questionSequence < 5)
+            {
+                questionSequence++;
+                points++;
+                QuestionSelect();
+                OnPointsChanged(nameof(DisplayPoints));
+                OnQuestionChanged(nameof(DisplayNextQuestion));
+            }
+            else
+            {
+                CharacterResult();
+                OnPointsChanged(nameof(DisplayCharacter));
+            }
+
+
+        }
+
+        //2 of 2 methods called by commands
+        void LeavePoints()
+        {
+            if (questionSequence < 5)
+            {
+                questionSequence++;
+                QuestionSelect();
+                OnPointsChanged(nameof(DisplayPoints));
+                OnQuestionChanged(nameof(DisplayNextQuestion));
+            }
+            else
+            {
+                CharacterResult();
+                OnPointsChanged(nameof(DisplayCharacter));
+            }
+        }
+
+
+        public string DisplayNextQuestion => $"{currentQuestion}";
+
+        public string DisplayPoints => $"You clicked {points} times(s).";
+
+        public string DisplayCharacter => $"{imageUrl}";
+
+
+        public void CharacterResult()
+        {
+
+            switch (points)
+            {
+                case 1:
+                    imageUrl = "fry.gif";
+                    break;
+                case 2:
+                    imageUrl = "fry.gif";
+                    break;
+                case 3:
+                    imageUrl = "prof.jpg";
+                    break;
+                case 4:
+                    imageUrl = "bender.jpg";
+                    break;
+                case 5:
+                    imageUrl = "zoidberg.jpg";
+                    break;
+                default:
+                    imageUrl = "zoidberg.jpg";
+                    break;
+            }
+
+        }
+
+
+        public void QuestionSelect()
+        {
+
+            string question = "";
+
+            switch (questionSequence)
+            {
+                case 1:
+                    question = "Would you drink slurm?";
+                    break;
+                case 2:
+                    question = "Do you like science and good news?";
+                    break;
+                case 3:
+                    question = "Do you like money/cigars?";
+                    break;
+                case 4:
+                    question = "Do you like zoidberg?";
+                    break;
+                case 5:
+                    question = "Do you think you know which character you are?";
+                    break;
+
+                default: break;
+            }
+
+            currentQuestion = question;
+
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPointsChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        void OnQuestionChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+    }
+}
+
