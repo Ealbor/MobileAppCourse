f4d5f527164336b31aa4c88fbb82b5be6d1e80f5
Eric <ealbor@my.wctc.edu>
2019-10-22 21:28:33 -0500

    Carousel
    The carousel is found within item details page. When you swipe to the right, you should see a red box view.
    The only thing I had to change to get this to work was  change the base class within a model.

---
+++ Carousel/Carousel/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Carousel.Models;
+
+namespace Carousel.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
