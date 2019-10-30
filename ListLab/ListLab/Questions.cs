671bf29b6aa09cc0b64efab71d5f3d05cf23e4df
Eric <ealbor@my.wctc.edu>
2019-10-30 18:30:32 -0500

    List Lab

---
+++ ListLab/ListLab/Questions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListLab
+{
+    public class Questions
+    {
+        public string Ques { get; set; }
+       
+
+        public override string ToString()
+        {
+            return Ques;
+        }
+
+    }
+}
